c4f8f028cfff4f8d3dd3d2b4408a894d0662a59e
Ryan Hashem <ryanohashem@yahoo.com>
2025-03-23 19:00:05 -0500

    can now accept CLI arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include "new.hpp"
 
-//handles inputs and outputs
-int main() {
+using std::cout, std::endl;
 
-    std::cout << "zet app" << std::endl;
+//allows CLI inputs
+//argc stores the number of parameters, argv is an array of c-strings
+int main(int argc, char* argv[]) {
+
+    //prints out number of CLI arguments inputted (including the executable)
+    cout << "number of arguments: " << argc << endl;
+
+    //prints out each argument
+    for (int i = 0; i < argc; i++) {
+        cout << "argument " << (i + 1) << ": " << argv[i] << endl; 
+    }
+
     return 0;
 }
