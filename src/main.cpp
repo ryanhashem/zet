de1ab5535804f2b56777c34fc09ec15cf625956f
Ryan Hashem <ryanohashem@yahoo.com>
2025-03-24 17:41:49 -0500

    added more parsing logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <tuple>
 #include "newNote.hpp"
 
-using std::cout, std::endl, std::string;
+using std::cout, std::endl, std::string, std::vector, std::tuple;
 
 //allows CLI inputs
 //argc stores the number of parameters, argv is an array of c-strings
@@ -30,14 +31,27 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    //if it reaches here:
-    //there are at least 2 arguments, the executable is named zettal, and it is a valid function call.
+    //vector of tuples to hold each flag and it's input
+    vector<tuple<string, string>> flags;
 
-    //have a for loop iterate through the flags
-    /*
+    //loops through the flags
     for (int i = 2; i < argc; i++) {
 
-    }*/
+        //inserts flag
+        if ((i % 2) == 0) {
+
+            string flag = argv[i];
+            string flag_input = argv[i + 1];
+
+            //adds new flag to list
+            flags.push_back({flag, flag_input});
+        }
+    }
+
+    //test
+    for (int i = 0; i < flags.size(); i++) {
+        cout << "(" << std::get<0>(flags.at(i)) << ", " << std::get<1>(flags.at(i)) << ")" << endl;
+    }
 
     return 0;
 }
